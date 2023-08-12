d67481b560685fee9e6c39ff63d4db982e11acf8
Michael <mike.shipleyland@gmail.com>
2023-08-11 23:26:11 -0600

    Stable Version: Missing Update to database, New Cards, Delete turning into inactive

--- database-seed.sql
+++ database-seed.sql
@@ -11,7 +11,7 @@ INSERT INTO employees(name, title, avatarurl) VALUES
  ('Chewbacca', 'Mechanic','https://podfanz.com/wp-content/uploads/2019/06/chewbacca_photograph_square_sticker-r1adf54f271d94bba90fc477a6d213997_0ugmc_8byvr_699.jpg'),
  ('Yoda', 'Jedi Master', 'https://i.imgflip.com/3wxdg2.png'),
  ('Obi-Wan Kenobi', 'Jedi Master','https://staticg.sportskeeda.com/editor/2022/06/1e4c9-16556208946670-1920.jpg'),
- ('Darth Vader', 'Dark Lord of the Sith', "https://lambsivy.com/cdn/shop/products/darth_lovey_detail_1024x1024.jpg"),
- ('Luke Skywalker', 'Jedi Knight', "https://static.wikia.nocookie.net/starwarscardtrader/images/0/0c/LukeSkywalkerJedi-Base1-front.png"),
- ('Leia Organa', 'Princess of Alderaan', "https://static.wikia.nocookie.net/disney-star-wars/images/e/e9/Leia_Endor_PROMO.png"),
- ('Han Solo', 'Smuggler', "https://d33wubrfki0l68.cloudfront.net/cb29bdbd3117fc389b6c04b294a52e908fa2e025/93c4e/_images/square-how-much-did-han-solo-owe.jpg");
+ ('Darth Vader', 'Dark Lord of the Sith', 'https://i.imgflip.com/3wxdg2.png'),
+ ('Luke Skywalker', 'Jedi Knight', 'https://i.imgflip.com/3wxdg2.png'),
+ ('Leia Organa', 'Princess of Alderaan', 'https://static.wikia.nocookie.net/disney-star-wars/images/e/e9/Leia_Endor_PROMO.png'),
+ ('Han Solo', 'Smuggler', 'https://d33wubrfki0l68.cloudfront.net/cb29bdbd3117fc389b6c04b294a52e908fa2e025/93c4e/_images/square-how-much-did-han-solo-owe.jpg');
